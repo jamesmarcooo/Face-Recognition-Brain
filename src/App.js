2adb9b7cb8a3cb568b711c5f0d655c1796a5ff4a
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-07 17:48:03 +0800

    Added Logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import 'tachyons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navigation />
+          <Logo />
+          <ImageLinkForm />
+           {/*<FaceRecognition />*/}
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
