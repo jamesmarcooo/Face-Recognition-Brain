b0b5bebbb3c644d37932da419faba68666a825ef
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-08 10:20:52 +0800

    Face Recognition Added API Clarifai

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Face-Recognition-Brain
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), Particle React.js, and used [Clarifai](https://www.clarifai.com/) API for Face Detection that locate faces with bounding boxes.
--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-// import Clarifai from 'clarifai';
-// import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Navigation from './components/Navigation/Navigation';
-// import Signin from './components/Signin/Signin';
-// import Register from './components/Register/Register';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Navigation from './Components/Navigation/Navigation';
+// import Signin from './Components/Signin/Signin';
+// import Register from './Components/Register/Register';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: 'a512731d6361449eba5b6f13466fb956'
+});
 
 const particlesOptions = {
   particles: {
@@ -22,21 +27,103 @@ const particlesOptions = {
   }
 }
 
-class App extends Component{
-  render(){
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      
+    }
+  }
+
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const {imageUrl, box } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
+         <Particles className='particles'
           params={particlesOptions}
         />
-        <header className="App-header">
-          <Navigation />
+
+        <div>
           <Logo />
           <Rank />
-          <ImageLinkForm />
-           {/*<FaceRecognition />*/}
-  
-        </header>
+          <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </div>
+
+        
       </div>
     );
   }
